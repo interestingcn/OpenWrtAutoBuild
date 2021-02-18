6b5f0d3983425022eacbc06027259d37f339a051
Melon <interestingcn@qq.com>
2021-02-18 19:31:44 +0800

    更换vssr加载位置

--- Newifi3-Lean-withVssr/custom1.sh
+++ Newifi3-Lean-withVssr/custom1.sh
@@ -2,6 +2,7 @@
 #=================================================
 # Author: @Meloncn
 # OpenWrtAutoBuild 固件编译自动化程序
+# Lean 固件使用
 #=================================================
 # 克隆远程代码结束，update Feeds前运行此脚本
 #
@@ -18,3 +19,11 @@
 # 脚本当前执行目录
 echo ”脚本当前执行目录“
 pwd
+
+# Helloworld 插件
+echo "正在下载插件：Hello World"
+git clone https://github.com/jerrykuku/lua-maxminddb.git ./gittmp
+mv -f gittmp/ ./package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git ./gittmp
+mv -f gittmp/ ./package/lean/luci-app-vssr
+echo "插件下载完成：Hello World"
